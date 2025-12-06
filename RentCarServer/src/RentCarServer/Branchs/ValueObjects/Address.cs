c1088eaa2a5caa5d1edee92d3ac28ac7e86d8145
Eren Birsin <erenbirsin@hotmail.com>
2025-12-06 12:51:43 +0300

    Branch model ve repository eklendi

---
+++ RentCarServer/src/RentCarServer/Branchs/ValueObjects/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace RentCarServer.Domain.Branchs.ValueObjects;
+
+public sealed record Address(
+    string City,
+    string District,
+    string FullAddress,
+    string PhoneNumber1,
+    string PhoneNumber2,
+    string Email);
