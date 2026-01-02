32fe922b7df208f6105968adf3b961dadb951ae1
Eren Birsin <erenbirsin@hotmail.com>
2026-01-02 10:02:06 +0300

    Angularda Extra model ve CRUD işlemleri yapıldı

--- RentCarClient/apps/admin/src/navigation.ts
+++ RentCarClient/apps/admin/src/navigation.ts
@@ -9,22 +9,22 @@ export interface NavigationModel{
 
 export const navigations: NavigationModel[]=[
   {
-    title:'Dashboard',
-    url: '/',
-    icon:'bi-speedometer2',
-    permission:'dashboard:view'
+        title:'Dashboard',
+        url: '/',
+        icon:'bi-speedometer2',
+        permission:'dashboard:view'
   },
   {
-    title :'Şubeler',
-    url:'/branches',
-    icon: 'bi-buildings',
-    permission:'branch:view'
+        title :'Şubeler',
+        url:'/branches',
+        icon: 'bi-buildings',
+        permission:'branch:view'
   },
   {
-    title:'Roller',
-    url:'/roles',
-    icon:'bi-clipboard2-check',
-    permission:'role:view'
+        title:'Roller',
+        url:'/roles',
+        icon:'bi-clipboard2-check',
+        permission:'role:view'
   },
   {
         title: 'Kategoriler',
@@ -39,9 +39,15 @@ export const navigations: NavigationModel[]=[
         permission: 'protection_package:view'
   },
   {
-    title:'Kullanıcılar',
-    url:'/users',
-    icon:'bi-people',
-    permission:'user:view'
-  }
+         title:'Kullanıcılar',
+         url:'/users',
+         icon:'bi-people',
+         permission:'user:view'
+  },
+  {
+        title: 'Ekstralar',
+        url: '/extra',
+        icon: 'bi-plus-square',
+        permission: 'extra:view'
+  },
 ]
