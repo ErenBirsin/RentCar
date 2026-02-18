0485f976fbbeb27527203a8287f89b440a14341a
Eren Birsin <erenbirsin@hotmail.com>
2026-02-18 15:14:39 +0300

    Ana sayfada giriş yapan kullanıcı için giriş/kayıt alanları gizlendi

--- RentCarClient/apps/ui/src/pages/home/home.ts
+++ RentCarClient/apps/ui/src/pages/home/home.ts
@@ -1,14 +1,14 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation, computed, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthState } from '../services/auth.state';
 
 @Component({
-  imports: [
-    RouterLink
-  ],
+  imports: [RouterLink],
   templateUrl: './home.html',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class Home {
-
+  readonly #authState = inject(AuthState);
+  readonly isLoggedIn = computed(() => !!this.#authState.token());
 }
